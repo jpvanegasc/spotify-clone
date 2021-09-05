02ebef78dfb14a1bc0e36edf365adde241e1040e
Juan Pablo Vanegas <juan.vanegas@tektontech.com>
2021-09-05 13:35:12 -0500

    Feat: Add alembic

---
+++ alembic/versions/1e97456cee28_first_migration.py
@@ -0,0 +1,24 @@
+"""first_migration
+
+Revision ID: 1e97456cee28
+Revises: 
+Create Date: 2021-09-05 07:09:22.395479
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '1e97456cee28'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
