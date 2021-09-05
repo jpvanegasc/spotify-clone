3f7a8e7a4714d2f0fc85d22fd04f3a8961a49475
Juan Pablo Vanegas <juan.vanegas@tektontech.com>
2021-09-05 08:17:48 -0500

    Feat: Update main.py

--- app/main.py
+++ app/main.py
@@ -1,10 +1,28 @@
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
 
-from .config import PROJECT_NAME, VERSION, DATABASE_URL
+from sqlalchemy import create_engine
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session, sessionmaker
 
-app = FastAPI(title=PROJECT_NAME, version=VERSION)
+# from .config import DATABASE_URL
+
+from .routers import user
+from .database import engine
+import app.models.user as user_models
+import app.models.music as music_models
+
+user_models.Base.metadata.create_all(bind=engine)
+music_models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+app.include_router(user.user_router)
+app.include_router(user.playlist_router)
 
 @app.get("/")
 def hello_world():
     return "hello world"
+
+
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
