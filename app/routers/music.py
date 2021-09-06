d2159bce8836fca854cef5162876c3514608311c
Juan Pablo Vanegas <juan.vanegas@tektontech.com>
2021-09-05 23:44:55 -0500

    Refactor: Remove unused functionality

--- app/routers/music.py
+++ app/routers/music.py
@@ -30,28 +30,28 @@ def get_db():
 
 # Artists
 
-@artist_router.post('/artists', response_model=schemas.Artist)
+@artist_router.post('/artists')
 def create_artist(artist: schemas.ArtistCreate, db: Session=Depends(get_db)):
     artist = crud.get_artist_by_name(db=db, artist_name=artist.name)
     if bool(artist):
         raise HTTPException(status_code=400, detail="artist already exists")
     return crud.create_artist(db=db, artist=artist)
 
-@artist_router.get('/artists/{artist_id}', response_model=schemas.Artist)
+@artist_router.get('/artists/{artist_id}')
 def read_artist(artist_id: int, db: Session=Depends(get_db)):
     artist = crud.get_artist(db=db, artist_id=artist_id)
     if artist is None:
         raise HTTPException(status_code=404, detail="artist not found")
     return artist
 
-@artist_router.get('/artists', response_model=schemas.Artist)
+@artist_router.get('/artists')
 def read_artists(skip: Optional[int]=0, limit: Optional[int]=100, db: Session=Depends(get_db)):
     artists = crud.get_artists(db=db, skip=skip, limit=limit)
     if not bool(artists):
         raise HTTPException(status_code=404, detail="artists not found")
     return artists
 
-@artist_router.put('/artists/{artist_id}', response_model=schemas.ArtistCreate)
+@artist_router.put('/artists/{artist_id}')
 def update_artist(artist_id: int, artist: schemas.ArtistCreate, db: Session=Depends(get_db)):
     db_artist = crud.get_artist(db=db, artist_id=artist_id)
     if artist is None:
@@ -72,28 +72,28 @@ def delete_artist(artist_id: int, db: Session=Depends(get_db)):
 
 # Albums
 
-@album_router.post('/albums', response_model=schemas.Album)
+@album_router.post('/albums')
 def create_album(album: schemas.AlbumCreate, db: Session=Depends(get_db)):
     album = crud.get_album_by_spotify_id(db=db, album_id=album.id)
     if bool(album):
         raise HTTPException(status_code=400, detail="album already exists")
     return crud.create_album(db=db, album=album)
 
-@album_router.get('/albums/{album_id}', response_model=schemas.Album)
+@album_router.get('/albums/{album_id}')
 def read_album(album_id: int, db: Session=Depends(get_db)):
     album = crud.get_album(db=db, album_id=album_id)
     if album is None:
         raise HTTPException(status_code=404, detail="album not found")
     return album
 
-@album_router.get('/albums', response_model=schemas.Album)
+@album_router.get('/albums')
 def read_albums(skip: Optional[int]=0, limit: Optional[int]=100, db: Session=Depends(get_db)):
     albums = crud.get_albums(db=db, skip=skip, limit=limit)
     if not bool(albums):
         raise HTTPException(status_code=404, detail="albums not found")
     return albums
 
-@album_router.put('/albums/{album_id}', response_model=schemas.AlbumCreate)
+@album_router.put('/albums/{album_id}')
 def update_album(album_id: int, album: schemas.AlbumCreate, db: Session=Depends(get_db)):
     db_album = crud.get_album(db=db, album_id=album_id)
     if album is None:
@@ -114,28 +114,28 @@ def delete_album(album_id: int, db: Session=Depends(get_db)):
 
 # Tracks
 
-@track_router.post('/tracks', response_model=schemas.Track)
+@track_router.post('/tracks')
 def create_track(track: schemas.TrackCreate, db: Session=Depends(get_db)):
     track = crud.get_track_by_spotify_id(db=db, track_id=track.id)
     if bool(track):
         raise HTTPException(status_code=400, detail="track already exists")
     return crud.create_track(db=db, track=track)
 
-@track_router.get('/tracks/{track_id}', response_model=schemas.Track)
+@track_router.get('/tracks/{track_id}')
 def read_track(track_id: int, db: Session=Depends(get_db)):
     track = crud.get_track(db=db, track_id=track_id)
     if track is None:
         raise HTTPException(status_code=404, detail="track not found")
     return track
 
-@track_router.get('/tracks', response_model=schemas.Track)
+@track_router.get('/tracks')
 def read_tracks(skip: Optional[int]=0, limit: Optional[int]=100, db: Session=Depends(get_db)):
     tracks = crud.get_tracks(db=db, skip=skip, limit=limit)
     if not bool(tracks):
         raise HTTPException(status_code=404, detail="tracks not found")
     return tracks
 
-@track_router.put('/tracks/{track_id}', response_model=schemas.TrackCreate)
+@track_router.put('/tracks/{track_id}')
 def update_track(track_id: int, track: schemas.TrackCreate, db: Session=Depends(get_db)):
     db_track = crud.get_track(db=db, track_id=track_id)
     if track is None:
@@ -154,7 +154,7 @@ def delete_track(track_id: int, db: Session=Depends(get_db)):
     track.commit()
     return {}
 
-@track_router.patch('playlists/{playlist_id}/tracks/{track_id}', response_model=schemas.Track)
+@track_router.patch('playlists/{playlist_id}/tracks/{track_id}')
 def add_track_to_playlist(playlist_id: int, track_id: int, db: Session=Depends(get_db)):
     playlist = user_crud.get_playlist(db=db, playlist_id=playlist_id)
     track = crud.get_track(db=db, track_id=track_id)
--- app/routers/user.py
+++ app/routers/user.py
@@ -26,24 +26,24 @@ def get_db():
 
 # Users
 
-@user_router.post('/users', response_model=schemas.User)
+@user_router.post('/users')
 def create_user(user:schemas.UserCreate, db: Session=Depends(get_db)):
     db_user = crud.get_user_by_email(db=db, user_email=user.email)
     if bool(db_user):
         raise HTTPException(status_code=400, detail="email already registered")
     return crud.create_user(db=db, user=user)
 
-@user_router.get('/users/{user_id}', response_model=schemas.User)
+@user_router.get('/users/{user_id}')
 def read_user(user_id: int, db: Session=Depends(get_db)):
     db_user = crud.get_user(db=db, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="user not found")
     return db_user
 
-@user_router.get('/users/find', response_model=schemas.User)
+@user_router.get('/users/find')
 def find_user(db: Session=Depends(get_db), email: Optional[str]=None, spotify_id: Optional[str]=None):
     if email is not None:
-        db_user = crud.get_user_by_email(db, email=email)
+        db_user = crud.get_user_by_email(db, user_email=email)
     elif spotify_id is not None:
         db_user = crud.get_user_by_spotify_id(db, user_id=spotify_id)
     else:
@@ -53,14 +53,14 @@ def find_user(db: Session=Depends(get_db), email: Optional[str]=None, spotify_id
         raise HTTPException(status_code=404, detail="user not found")
     return db_user
 
-@user_router.get('/users', response_model=schemas.User)
+@user_router.get('/users')
 def read_users(skip: Optional[int]=0, limit: Optional[int]=100, db: Session=Depends(get_db)):
     db_users = crud.get_users(db, skip=skip, limit=limit)
     if not bool(db_users):
         raise HTTPException(status_code=404, detail="users not found")
     return db_users
 
-@user_router.put('/users/{user_id}', response_model=schemas.UserCreate)
+@user_router.put('/users/{user_id}')
 def update_user(user_id: int, user: schemas.UserCreate, db: Session=Depends(get_db)):
     db_user = crud.get_user(db=db, user_id=user_id)
     if user is None:
@@ -81,28 +81,28 @@ def delete_user(user_id: int, db: Session=Depends(get_db)):
 
 # Playlists
 
-@playlist_router.post('/playlists', response_model=schemas.Playlist)
-def create_playlist(playlist:schemas.PlaylistCreate, user_id: int, db: Session=Depends(get_db)):
-    db_playlist = crud.get_playlist_by_spotify_id(db, user_id=playlist.id)
+@playlist_router.post('/playlists')
+def create_playlist(playlist:schemas.PlaylistCreate, db: Session=Depends(get_db)):
+    db_playlist = crud.get_playlist_by_spotify_id(db, playlist_id=playlist.id)
     if bool(db_playlist):
         raise HTTPException(status_code=400, detail="playlist already exists")
     return crud.create_playlist(db=db, playlist=playlist)
 
-@playlist_router.get('/playlists/{playlist_id}', response_model=schemas.Playlist)
+@playlist_router.get('/playlists/{playlist_id}')
 def read_playlist(playlist_id: int, db: Session=Depends(get_db)):
     db_playlist = crud.get_playlist(db=db, playlist_id=playlist_id)
     if db_playlist is None:
         raise HTTPException(status_code=404, detail="playlist not found")
     return db_playlist
 
-@playlist_router.get('/users/{user_id}/playlists', response_model=schemas.Playlist)
+@playlist_router.get('/users/{user_id}/playlists')
 def get_user_playlists(user_id:int, db: Session=Depends(get_db)):
     db_playlists = crud.get_playlists_by_user(db=db, user_id=user_id)
     if not bool(db_playlists):
         raise HTTPException(status_code=404, detail="playlists not found")
     return db_playlists
 
-@playlist_router.put('/playlists/{playlist_id}', response_model=schemas.PlaylistCreate)
+@playlist_router.put('/playlists/{playlist_id}')
 def update_playlist(playlist_id: int, playlist: schemas.PlaylistCreate, db: Session=Depends(get_db)):
     db_playlist = crud.get_playlist(db=db, playlist_id=playlist_id)
     if playlist is None:
